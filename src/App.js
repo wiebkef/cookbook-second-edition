933c7e2c6cad2ec46ada9baac21dc335811379bf
Wiebke <wiebke.feldt@gmail.com>
2023-02-08 16:05:24 +0100

    has env vars in env file

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ var contentful = require("contentful");
 
 function App() {
   // this is just some spaceholder stuff until we have the actual contentful schemes and contents
-
+  const SPACE_ID = "g8l2kfm077sr";
+  const ENVIRONMENT_ID = "master";
+  const ACCESS_TOKEN = "9EjIonfJOKdfIZqZEJ1F3JW45WYFlGFZGt89iJ3fP5c";
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const client = contentful.createClient({
-      space: process.env.REACT_APP_SPACE_ID,
-      environment: process.env.REACT_APP_ENVIRONMENT_ID,
-      accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+      space: SPACE_ID,
+      environment: ENVIRONMENT_ID,
+      accessToken: ACCESS_TOKEN,
     });
 
     client
