406993c7c0a206182b00cd95b1f06c88a76c8b47
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-02-14 21:42:49 +0100

    Categories names

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -22,19 +22,19 @@ function Categories() {
             <div className='col-xs-12 col-sm-12 col-md-6 col-lg-3'>
             <div className='images'>
                 <img src={beef} alt="Beef" />
-                <h3>Pork</h3>
+                <h3>Beef</h3>
                 </div>
             </div>
             <div className='col-xs-12 col-sm-12 col-md-6 col-lg-3'>
             <div className='images'>
                 <img src={chicken} alt="Chicken" />
-                <h3>Pork</h3>
+                <h3>Chicken</h3>
                 </div>
             </div>
             <div className='col-xs-12 col-sm-12 col-md-6 col-lg-3'>
             <div className='images'>
                 <img src={fish} alt="Fish" />
-                <h3>Pork</h3>
+                <h3>Fish</h3>
                 </div>
             </div>
           </div>
