fa8f4c424e160765903afcdff1183fae958e38d0
Wiebke <wiebke.feldt@gmail.com>
2023-02-16 17:16:43 +0100

    some margins

--- src/components/PostShort.js
+++ src/components/PostShort.js
@@ -23,14 +23,14 @@ function PostShort({ recipe, postColor }) {
   return (
     <div className={`${colorClass} d-flex postShortCard`}>
       <div className="row justify-content-start">
-        <div className="col-3  p-0">
+        <div className="col-4  p-0">
           <img
             src={recipe.fields.thumbnail.fields.file.url}
             className="img-fluid d-block start"
             alt={recipe.fields.image.fields.description}
           />
         </div>
-        <div className="col-9">
+        <div className="col-8">
           <div className="d-flex flex-col justify-content-end">
             <div className="iconsFirstShort-wrapper p-3">
               <span class="material-symbols-outlined">schedule</span>
@@ -45,10 +45,13 @@ function PostShort({ recipe, postColor }) {
               {new Date(recipe.fields.date).toLocaleDateString()}
             </div>
           </div>
-          <div className="lora-font text-start ps-3">
+          <div className="lora-font text-start ps-3 mt-4 d-flex flex-column">
             <h2>{recipe.fields.title}</h2>
             <p>{recipe.fields.description.content[0].content[0].value}</p>
-            <a className="btn btn-primary" href={`/recipes/${recipe.sys.id}`}>
+            <a
+              className="btn btn-primary col-3 "
+              href={`/recipes/${recipe.sys.id}`}
+            >
               Read more
             </a>
           </div>
