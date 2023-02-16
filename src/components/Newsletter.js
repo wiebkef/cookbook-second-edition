c23b3397944cd815db332c0a1a4268f4ff8234c5
KimsandraO <kimsandra08@gmail.com>
2023-02-16 17:24:54 +0100

    essage

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -8,14 +8,14 @@ const Newsletter = () => {
       <div className='container d-flex justify-content-evenly'>
         <div className='row'>
 
-          <div className='col'>
-            <p>Receive Updates by subscribing for our Newsletter</p>
+          <div className='subscribe-button'>
+            <p>Receive Updates by Subscribing for our Newsletter</p>
             <input type='email' className='input' placeholder='Email'/>
             <br />
             <input type='password' className='input' placeholder='password'/>
             <br />
             <button type='submit' className='button  background-color: #C7CEEA!important;'> Subscribe </button>
-
+          
           </div>
 
         </div>
